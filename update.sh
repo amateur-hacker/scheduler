a952debf839306ad299c44cbdfdde5533fcfa3a6
amateur_hacker <hackerhacked262004@gmail.com>
2023-07-03 03:55:54 +0530

    Made changes to my update.sh script

--- update.sh
+++ update.sh
@@ -10,5 +10,6 @@
 
 # Upcoming features in the scheduler:
 
-# 1. Key mappings will be available in the scheduler.
-# 2. You will be able to add more than 10 schedules per day.
+# 1. GUI version of the scheduler will be available soon.
+# 2. Key mappings will be available in the scheduler.
+# 3. You will be able to add more than 10 schedules per day.
